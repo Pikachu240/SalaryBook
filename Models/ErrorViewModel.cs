efed95181143cdc0bf8e058b80d7a3b31e507deb
Pikachu240 <156485856+Pikachu240@users.noreply.github.com>
2026-01-13 12:58:19 +0530

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GalaxyBookWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
